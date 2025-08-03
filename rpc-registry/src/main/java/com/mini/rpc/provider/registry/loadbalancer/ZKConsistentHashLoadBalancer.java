76fe70205234887e0f4da8d4034a85a089bcfcad
ld <1815720665@qq.com>
2025-08-03 15:20:03 +0800

    一致性哈希

--- rpc-registry/src/main/java/com/mini/rpc/provider/registry/loadbalancer/ZKConsistentHashLoadBalancer.java
+++ rpc-registry/src/main/java/com/mini/rpc/provider/registry/loadbalancer/ZKConsistentHashLoadBalancer.java
@@ -11,6 +11,9 @@ public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<Service
     private final static int VIRTUAL_NODE_SIZE = 10;
     private final static String VIRTUAL_NODE_SPLIT = "#";
 
+    /**
+     * 选择服务实例: 一致性哈希算法
+     */
     @Override
     public ServiceInstance<ServiceMeta> select(List<ServiceInstance<ServiceMeta>> servers, int hashCode) {
         TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = makeConsistentHashRing(servers);
@@ -25,16 +28,24 @@ public class ZKConsistentHashLoadBalancer implements ServiceLoadBalancer<Service
         return entry.getValue();
     }
 
+    /**
+     * 构建一致性哈希环<br>
+     * 每个服务实例对应多个虚拟节点，增加负载均衡的均匀性
+     */
     private TreeMap<Integer, ServiceInstance<ServiceMeta>> makeConsistentHashRing(List<ServiceInstance<ServiceMeta>> servers) {
         TreeMap<Integer, ServiceInstance<ServiceMeta>> ring = new TreeMap<>();
         for (ServiceInstance<ServiceMeta> instance : servers) {
             for (int i = 0; i < VIRTUAL_NODE_SIZE; i++) {
+                // 构建虚拟节点的唯一标识 "addr:port#i".hashCode()
                 ring.put((buildServiceInstanceKey(instance) + VIRTUAL_NODE_SPLIT + i).hashCode(), instance);
             }
         }
         return ring;
     }
 
+    /**
+     * 构建服务实例的唯一标识 "addr:port"
+     */
     private String buildServiceInstanceKey(ServiceInstance<ServiceMeta> instance) {
         ServiceMeta payload = instance.getPayload();
         return String.join(":", payload.getServiceAddr(), String.valueOf(payload.getServicePort()));
